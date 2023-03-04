d3080333ef64e6210b3600b47c2657946750d857
Filip <23342492+FilipOrzechowski@users.noreply.github.com>
2023-03-04 18:12:12 +0100

    Method printing the grid added

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -31,3 +31,15 @@ class Grid():
         self.p2_wins = 0
 
         self.win_combinations =[["A1", "A2", "A3"], ["B1", "B2", "B3"], ["C1", "C2", "C3"], ["A1", "B2", "C3"], ["A3", "B2", "C1"], ["A1", "B1", "C1"], ["A2", "B2", "C2"], ["A3", "B3", "C3"]]
+
+# This method is used to print the grid
+    def print_grid(self):
+        self.row_A = "|  A  |  {A1}  |  {A2}  |  {A3}  |".format(A1=self.a1, A2=self.a2, A3=self.a3)
+        self.row_B = "|  B  |  {B1}  |  {B2}  |  {B3}  |".format(B1=self.b1, B2=self.b2, B3=self.b3)
+        self.row_C = "|  C  |  {C1}  |  {C2}  |  {C3}  |".format(C1=self.c1, C2=self.c2, C3=self.c3)
+        self.row_list = [self.row_A, self.row_B, self.row_C]
+        print(self.row_head)
+        print(self.row_line)
+        for row in self.row_list:
+            print(row)
+            print(self.row_line)
